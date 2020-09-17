ebea97fd35af7a09a4989fac51586981bf595dc5
TarunVyda6 <kamaltarun.rao6@gmail.com>
2020-09-17 21:11:20 +0530

    class method feature is added

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -10,45 +10,50 @@ public class EmpWageComputation
 	public static final int numberOfWorkingHours=100;
 	public static void main(String[] args)
 	{
-		int workingHr=0;
-		int totalSalary=0;
-		Random random=new Random();
-		int employeeType=random.nextInt(3);
-		int totalWorkingHrs=0;
-		for(int day=1;day<=numberOfWorkingDays && totalWorkingHrs<=numberOfWorkingHours;day++)
-		{
-			switch(employeeType)
-			{
-                		case isFullTime:
-                        			workingHr=8;
-                        			break;
-	                	case isPartTime:
-			                        workingHr=4;
-			                        break;
-			     	default:
-			                        workingHr=0;
-	                }
-			if(totalWorkingHrs==96||totalWorkingHrs==94)
-			{
-				if (workingHr==8)
-				{
-					break;
-				}
-			}
-			else if(totalWorkingHrs==98)
-			{
-				break;
-			}
-			totalWorkingHrs+=workingHr;
-			int dailyWage=wagePerHour*workingHr;
-			totalSalary+=dailyWage;
-		}
-		System.out.println("current month wage of employee is : "+totalSalary);
+		WageCalculation();
 	}
 
+	public static void WageCalculation()
+	{
+		int workingHr=0;
+                int totalSalary=0;
+                Random random=new Random();
+                int employeeType=random.nextInt(3);
+                int totalWorkingHrs=0;
+                for(int day=1;day<=numberOfWorkingDays && totalWorkingHrs<=numberOfWorkingHours;day++)
+                {
+                        switch(employeeType)
+                        {
+                                case isFullTime:
+                                                workingHr=8;
+                                                break;
+                                case isPartTime:
+                                                workingHr=4;
+                                                break;
+                                default:
+                                                workingHr=0;
+                        }
+                        if(totalWorkingHrs==96||totalWorkingHrs==94)
+                        {
+                                if (workingHr==8)
+                                {
+                                        break;
+                                }
+                        }
+                        else if(totalWorkingHrs==98)
+                        {
+                                break;
+                        }
+                        totalWorkingHrs+=workingHr;
+                        int dailyWage=wagePerHour*workingHr;
+                        totalSalary+=dailyWage;
+                }
+                System.out.println("current month wage of employee is : "+totalSalary);
+
+	}
 
 
 
 }
 
-		
+
